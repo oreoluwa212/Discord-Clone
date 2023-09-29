38188b880c1b35b89915949f325f55809054433b
Oreoluwa <oreoluwaajayyiruth@gmail.com>
2023-09-29 19:12:22 +0000

    Discord clone

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import discordLogo from "../assets/discord_main_logo.svg";
+function App() {
+  return (
+    <div style={{ backgroundColor: "#5865f2" }}>
+      <img src={discordLogo} alt="discord" />
+    </div>
+  );
+}
+
+export default App;
